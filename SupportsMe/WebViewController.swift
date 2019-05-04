ee1647e36a41f2d8cc6a48c5ba16bc29be0b69e8
Andrew Witte <wittea91@my.sunyulster.edu>
2019-05-03 23:01:53 -0400

    Map & Calendar working. As well as some layer 3 design.

---
+++ SupportsMe/WebViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WebViewController.swift
+//  SupportsMe
+//
+//  Created by Admin on 5/3/19.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class   ViewController: UIViewController {
+    
+    override func loadView() {
+    super.viewDidLoad()
+}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+}
+    
+    @IBAction func onChannelButtonWasPressed(_ sender: Any) {
+    }
+}
